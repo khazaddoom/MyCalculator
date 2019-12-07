76f6764b7b012b75c4eda0cd372755674d71ae47
khazaddoom <31121986g@gmail.com>
2019-12-07 17:15:08 +0530

    Added SCSS compilation option, calculator look

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Button } from 'protractor';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
